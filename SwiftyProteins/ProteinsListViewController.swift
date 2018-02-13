f3d7698dc6c38903e8d0d002875d5b90114dc713
Jeremy SCHOTTE <jschotte@e1r3p10.42.fr>
2018-02-13 13:38:57 +0100

    search bar OK

--- SwiftyProteins/ProteinsListViewController.swift
+++ SwiftyProteins/ProteinsListViewController.swift
@@ -8,14 +8,21 @@
 
 import UIKit
 
-class ProteinsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+class ProteinsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate
 {
     
     
     var ArrayProteins : [String] = []
     
-    override func viewDidLoad() {
+    var filtered : [String] = []
+    var isSearching = false
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        
+        searchBar.delegate = self
         
         let path = Bundle.main.path(forResource: "ligands", ofType: "txt")
         
@@ -32,27 +39,81 @@ class ProteinsListViewController: UIViewController, UITableViewDelegate, UITable
                 print("Error: \(error)")
             }
         }
+        
+        searchBar.returnKeyType = UIReturnKeyType.done
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return ArrayProteins.count
+    @IBOutlet weak var proteins: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        if (isSearching)
+        {
+            return filtered.count
+        }
+        else
+        {
+            return ArrayProteins.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        if (indexPath.row == 0)
-         {
-         let cell = tableView.dequeueReusableCell(withIdentifier: "search") as? SearchTableViewCell
-         return cell!
-         }
-         else
-         {
         let cell = tableView.dequeueReusableCell(withIdentifier: "protein")
-        cell?.textLabel?.text = ArrayProteins[indexPath.row - 1]
+        if (isSearching)
+        {
+            cell?.textLabel?.text = filtered[indexPath.row]
+        }
+        else
+        {
+            cell?.textLabel?.text = ArrayProteins[indexPath.row]
+        }
         return cell!
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String)
+    {
+    
+        if (searchBar.text == nil || searchBar.text == "")
+        {
+            isSearching = false
+            
+            view.endEditing(true)
+        }
+        else
+        {
+            isSearching = true
+            
+            filtered = ArrayProteins.filter({$0.contains(searchText)})
+            proteins.reloadData()
         }
     }
     
+    func tableView(_ tableView: UITableView,
+                   didSelectRowAt indexPath: IndexPath)
+    {
+        print("select row")
+        if (isSearching)
+        {
+            self.performSegue(withIdentifier: "showProtein", sender: filtered[indexPath.row])
+        }
+        else
+        {
+            self.performSegue(withIdentifier: "showProtein", sender: ArrayProteins[indexPath.row])
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        print("show protein \(String(describing: sender))")
+        if segue.identifier == "showProtein"
+        {
+            if segue.destination is ProteinViewController
+            {
+                
+            }
+        }
+    }
+
 }
 
 
